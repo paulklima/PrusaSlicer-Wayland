fd0fdd243db5ca8fe73063d4448b1ef16b1d1ace
paul <paul@klima.in>
2026-04-19 16:27:41 +0000

    feat: native Wayland support (GTK3 + EGL)
    Enable native Wayland support for PrusaSlicer on Linux by switching
    the build defaults from GTK2 to GTK3 with EGL for OpenGL rendering.
    
    Changes:
    - Default to GTK3 (DEP_WX_GTK3=ON) and enable EGL (wxUSE_GLCANVAS_EGL=ON)
    - Build GLEW with EGL support on Linux (GLEW_EGL=ON)
    - Guard gtk-menu-images property with #ifdef __WXGTK2__ (removed in GTK 3.10)
    - Remove forced GDK_BACKEND=x11 so GTK3 can auto-detect Wayland
    - Accept WAYLAND_DISPLAY for pure Wayland sessions without XWayland
    - Add -std=gnu11 for GMP build with GCC 13+

--- deps/+GMP/GMP.cmake
+++ deps/+GMP/GMP.cmake
@@ -18,7 +18,7 @@ if (MSVC)
 
 else ()
     string(TOUPPER "${CMAKE_BUILD_TYPE}" _buildtype_upper)
-    set(_gmp_ccflags "${CMAKE_CXX_FLAGS_${_buildtype_upper}} -fPIC -DPIC -Wall -Wmissing-prototypes -Wpointer-arith -pedantic -fomit-frame-pointer -fno-common")
+    set(_gmp_ccflags "${CMAKE_CXX_FLAGS_${_buildtype_upper}} -fPIC -DPIC -Wall -Wmissing-prototypes -Wpointer-arith -pedantic -fomit-frame-pointer -fno-common -std=gnu11")
     set(_gmp_build_tgt "${CMAKE_SYSTEM_PROCESSOR}")
 
     set(_cross_compile_arg "")
